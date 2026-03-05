af3273a254975d6b49d1929b1c3545bdb099a994
waisshaikh <waisshaikh394@gmail.com>
2026-03-05 16:48:58 +0530

    Token-BlackListing

--- backend/src/Routes/user.auth.js
+++ backend/src/Routes/user.auth.js
@@ -7,6 +7,6 @@ const router = Router()
 router.post('/register',authController.registerUser);
 router.post('/login',authController.loginUser);
 router.get("/get-me", authMiddleware.authUser, authController.getMe);
-router.get('/logout',authController.logoutUser)
+router.get('/logout',authController.logoutUser )
 
 module.exports=router
